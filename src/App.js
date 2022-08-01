328487a66185a3c8733ccf27cb344f9c0d83a1f9
白貓 <awacutewhitecat@gmail.com>
2022-08-01 16:20:58 +0800

    Blog Link Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import {
-    Header,
-    Avatar,
-    InsTag,
-    LinksWrapper,
-    Link,
-    Icon,
-    ButtonLinks,
-    Container,
-    Rights,
-    RightsWrapper,
+  Header,
+  Avatar,
+  InsTag,
+  LinksWrapper,
+  Link,
+  Icon,
+  ButtonLinks,
+  Container,
+  Rights,
+  RightsWrapper,
 } from "./AppElements";
 
 import AvatarIcon from "./images/WhiteCat.jpg";
@@ -19,54 +19,50 @@ import Github from "./images/github.svg";
 import PullToRefresh from "react-simple-pull-to-refresh";
 
 const handleRefresh = () => {
-    window.location.reload();
+  window.location.reload();
 };
 
 function App() {
-    return (
-        <PullToRefresh onRefresh={handleRefresh}>
-            <Container id="main">
-                <Header>
-                    <Avatar src={AvatarIcon} alt="avatar" />
-                    <InsTag href="https://www.instagram.com/whitecatstudy/">
-                        @ whitecatstudy
-                    </InsTag>
-                </Header>
-                <LinksWrapper>
-                    <ButtonLinks>
-                        <Link href="">
-                            <Icon src={HomePage} alt="homepage" width="22" />{" "}
-                            &thinsp; Blog (未完工)
-                        </Link>
-                    </ButtonLinks>
-                    <ButtonLinks>
-                        <Link href="https://drive.google.com/drive/folders/1AHssKp04aYFJLYQUMbb9ZXvjV1lm1j53?usp=sharing">
-                            <Icon src={Notes} alt="notes" width="23" /> &thinsp;
-                            筆記下載
-                        </Link>
-                    </ButtonLinks>
-                    <ButtonLinks>
-                        <Link href="https://izcc.ckcsc.net">
-                            <Icon src={Website} alt="izcc-website" width="18" />{" "}
-                            &thinsp; 暑訓網站
-                        </Link>
-                    </ButtonLinks>
-                    <ButtonLinks>
-                        <Link href="https://github.com/CuteWhiteCat/link-tree-react">
-                            <Icon src={Github} alt="github" width="20" />{" "}
-                            &thinsp; Github 專案
-                        </Link>
-                    </ButtonLinks>
-                </LinksWrapper>
-                <RightsWrapper>
-                    <Rights>
-                        WhiteCat © {new Date().getFullYear()} All rights
-                        reserved.
-                    </Rights>
-                </RightsWrapper>
-            </Container>
-        </PullToRefresh>
-    );
+  return (
+    <PullToRefresh onRefresh={handleRefresh}>
+      <Container id="main">
+        <Header>
+          <Avatar src={AvatarIcon} alt="avatar" />
+          <InsTag href="https://www.instagram.com/whitecatstudy/">
+            @ whitecatstudy
+          </InsTag>
+        </Header>
+        <LinksWrapper>
+          <ButtonLinks>
+            <Link href="https://whitecat.tk">
+              <Icon src={HomePage} alt="homepage" width="22" /> &thinsp; Blog
+            </Link>
+          </ButtonLinks>
+          <ButtonLinks>
+            <Link href="https://drive.google.com/drive/folders/1AHssKp04aYFJLYQUMbb9ZXvjV1lm1j53?usp=sharing">
+              <Icon src={Notes} alt="notes" width="23" /> &thinsp; 筆記下載
+            </Link>
+          </ButtonLinks>
+          <ButtonLinks>
+            <Link href="https://izcc.ckcsc.net">
+              <Icon src={Website} alt="izcc-website" width="18" /> &thinsp;
+              暑訓網站
+            </Link>
+          </ButtonLinks>
+          <ButtonLinks>
+            <Link href="https://github.com/CuteWhiteCat/link-tree-react">
+              <Icon src={Github} alt="github" width="20" /> &thinsp; Github 專案
+            </Link>
+          </ButtonLinks>
+        </LinksWrapper>
+        <RightsWrapper>
+          <Rights>
+            WhiteCat © {new Date().getFullYear()} All rights reserved.
+          </Rights>
+        </RightsWrapper>
+      </Container>
+    </PullToRefresh>
+  );
 }
 
 export default App;
